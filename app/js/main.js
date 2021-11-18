12250c96850357bc0d62790344aacf7e3bc37a55
Axel0000000 <ostinskyi@gmail.com>
2021-11-19 01:52:51 +0200

    added design

--- app/js/main.js
+++ app/js/main.js
@@ -7,6 +7,20 @@ $(function(){
       autoplay: true
    })
 
-   var mixer = mixitup('.products__gallery-items');
+   // var mixer1 = mixitup('.products__gallery-items');
+
+   // var mixer2 = mixitup('.design__gallery');
+
+   var containerEl1 = document.querySelector('[data-ref="container-1"]');
+   var containerEl2 = document.querySelector('[data-ref="container-2"]');
+  
+   var config = {
+     controls: {
+       scope: 'local'
+     }
+   };
+  
+   var mixer1 = mixitup(containerEl1, config);
+   var mixer2 = mixitup(containerEl2, config);
 
 });
