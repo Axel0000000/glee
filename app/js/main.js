bfc856197ac4ad3be04ef2e8ad7b9ecc5488a206
Axel0000000 <ostinskyi@gmail.com>
2021-11-26 20:24:45 +0200

    added product list

--- app/js/main.js
+++ app/js/main.js
@@ -7,6 +7,31 @@ $(function(){
       autoplay: true
    });
 
+   $(".filter-price__input").ionRangeSlider({
+      type: 'double',
+      prefix: '$',
+      
+      onStart: function (data) {
+         $('.filter-price__from').text(data.from);
+         $('.filter-price__to').text(data.to);
+      },
+      onChange: function (data) {
+         $('.filter-price__from').text(data.from);
+         $('.filter-price__to').text(data.to);
+     },
+
+   });
+
+   $(".star").rateYo({
+      rating: 3.6,
+      starWidth: "12px",
+      normalFill: "#d6d6d6",
+      ratedFill: "#ffcc00",
+      readOnly: true,
+      fullStar: true,
+      spacing: "4px",
+    });
+
    var containerEl1 = document.querySelector('[data-ref="container-1"]');
    var containerEl2 = document.querySelector('[data-ref="container-2"]');
   
